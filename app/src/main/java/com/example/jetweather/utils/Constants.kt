82186eb9a894a2f2b4a953780ab67094855474b0
anshh-raj <anshambastha@gmail.com>
2025-02-07 02:58:02 +0530

    updated constants

--- app/src/main/java/com/example/jetweather/utils/Constants.kt
+++ app/src/main/java/com/example/jetweather/utils/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.jetweather.utils
 
 object Constants {
+    // https://api.openweathermap.org/data/3.0/onecall?lat=28.7041&lon=77.1025&appid=fb059de8568e9e10e1c05fa8609deed8
     const val  BASE_URL = "https://api.openweathermap.org/"
     const val API_KEY = ""
 
